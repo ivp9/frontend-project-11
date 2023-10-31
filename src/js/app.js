17040c8bfe3e658b37cee3922be9e817227638ae
Ivan Pashkov <ivan_pashkov_99@mail.ru>
2023-11-01 00:45:13 +0300

    add createElements.js to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,12 @@ import { Modal } from 'bootstrap';
 import '../css/custom.css';
 import * as yup from 'yup';
 import i18next from 'i18next';
+import _ from 'lodash';
 import watch from './view.js';
 import ru from '../locales/index.js';
 import yupLocales from '../locales/yupLocales.js';
-import { updatePosts, createElements } from './createElements.js';
+import getDataFromUrl from './getDataFromUrl.js';
+import parseRssContent from './parsers.js';
 
 export default () => {
   const state = {
@@ -41,6 +43,69 @@ export default () => {
   const validate = (url, arrayOfUrls) => {
     const schema = yup.string().url().notOneOf(arrayOfUrls);
     return schema.validate(url);
+  };
+
+  const updatePosts = (watchedState) => {
+    const update = () => {
+      const updatedPosts = watchedState.feeds.map((feed) => getDataFromUrl(feed.link)
+        .then((response) => {
+          const { resultPosts } = parseRssContent(response, feed.link);
+
+          const newPosts = resultPosts.filter(
+            (post) => !watchedState.posts
+              .some((statePost) => statePost.link === post.link),
+          )
+            .map((post) => ({ ...post, id: _.uniqueId() }));
+
+          watchedState.posts = [...newPosts, ...watchedState.posts];
+        })
+        .catch((e) => {
+          console.log(e);
+        }));
+
+      Promise.all(updatedPosts).then(() => setTimeout(update, 5000));
+    };
+
+    update();
+  };
+
+  const webError = (watchedState) => {
+    watchedState.form.isValid = false;
+    watchedState.form.error = 'texts.statusMessage.webError';
+    watchedState.form.status = 'fail';
+  };
+
+  const novalidRssError = (watchedState) => {
+    watchedState.form.isValid = false;
+    watchedState.form.status = 'fail';
+    watchedState.form.error = 'texts.statusMessage.noValidRss';
+  };
+
+  const errorCatching = (e, watchedState) => {
+    if (e.isParsingError) {
+      novalidRssError(watchedState);
+    } else {
+      webError(watchedState);
+    }
+  };
+
+  const createElements = (url, watchedState) => {
+    getDataFromUrl(url)
+      .then((response) => {
+        const {
+          titleRSS, descriptionRSS, link, resultPosts,
+        } = parseRssContent(response, url);
+
+        watchedState.form.status = 'success';
+        watchedState.form.error = 'texts.statusMessage.successful';
+        watchedState.feeds.unshift({ titleRSS, descriptionRSS, link });
+
+        const posts = resultPosts.map((post) => ({ ...post, id: _.uniqueId() }));
+        watchedState.posts = [...posts, ...watchedState.posts];
+      })
+      .catch((e) => {
+        errorCatching(e, watchedState);
+      });
   };
 
   const modal = new Modal(elements.modal);
